0ddd14281888d04a61d5451804a69a3536fa91c3
Tvli <526864480@qq.com>
2014-07-05 01:40:06 +0100

    multi threads implement

---
+++ src/MultiThreads.java
@@ -0,0 +1,85 @@
+import org.encog.Encog;
+import org.encog.ml.data.MLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+import org.encog.util.benchmark.RandomTrainingFactory;
+
+/**
+ * Created by Teng on 05/07/2014.
+ */
+public class MultiThreads {
+    public static final int INPUT_COUNT = 40;
+    public static final int HIDDEN_COUNT = 60;
+    public static final int OUTPUT_COUNT = 20;
+
+    public static BasicNetwork generateNetwork()
+    {
+        final BasicNetwork network = new BasicNetwork();
+        network.addLayer(new BasicLayer(MultiThreads.INPUT_COUNT));
+        network.addLayer(new BasicLayer(MultiThreads.HIDDEN_COUNT));
+        network.addLayer(new BasicLayer(MultiThreads.OUTPUT_COUNT));
+        network.getStructure().finalizeStructure();
+        network.reset();
+        return network;
+    }
+
+    public static MLDataSet generateTraining()
+    {
+        final MLDataSet training = RandomTrainingFactory.generate(1000, 50000,
+                INPUT_COUNT, OUTPUT_COUNT, -1, 1);
+        return training;
+    }
+
+    public static double evaluateRPROP(BasicNetwork network,MLDataSet data)
+    {
+
+        ResilientPropagation train = new ResilientPropagation(network,data);
+        train.setThreadCount(1);
+        long start = System.currentTimeMillis();
+        System.out.println("Training 20 Iterations with RPROP");
+        for(int i=1;i<=20;i++)
+        {
+            train.iteration();
+            System.out.println("Iteration #" + i + " Error:" + train.getError());
+        }
+        train.finishTraining();
+        long stop = System.currentTimeMillis();
+        double diff = ((double)(stop - start))/1000.0;
+        System.out.println("RPROP Result:" + diff + " seconds." );
+        System.out.println("Final RPROP error: " + network.calculateError(data));
+        return diff;
+    }
+
+    public static double evaluateMPROP(BasicNetwork network,MLDataSet data)
+    {
+
+        ResilientPropagation train = new ResilientPropagation(network,data);
+        train.setThreadCount(0);
+        long start = System.currentTimeMillis();
+        System.out.println("Training 20 Iterations with MPROP");
+        for(int i=1;i<=20;i++)
+        {
+            train.iteration();
+            System.out.println("Iteration #" + i + " Error:" + train.getError());
+        }
+        train.finishTraining();
+        long stop = System.currentTimeMillis();
+        double diff = ((double)(stop - start))/1000.0;
+        System.out.println("MPROP Result:" + diff + " seconds." );
+        System.out.println("Final MPROP error: " + network.calculateError(data));
+        return diff;
+    }
+
+    public static void main(String args[])
+    {
+        BasicNetwork network = generateNetwork();
+        MLDataSet data = generateTraining();
+
+        double rprop = evaluateRPROP(network,data);
+        double mprop = evaluateMPROP(network,data);
+        double factor = rprop/mprop;
+        System.out.println("Factor improvement:" + factor);
+        Encog.getInstance().shutdown();
+    }
+}
